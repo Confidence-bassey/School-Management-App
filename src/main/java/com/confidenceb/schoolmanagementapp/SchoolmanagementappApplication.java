dd21bc64bdf77e93a01d9add83660b0d4de703dd
Confidence-bassey <confidencebass2@gmail.com>
2023-03-17 22:58:12 +0100

    first commit

---
+++ src/main/java/com/confidenceb/schoolmanagementapp/SchoolmanagementappApplication.java
@@ -0,0 +1,13 @@
+package com.confidenceb.schoolmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolmanagementappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolmanagementappApplication.class, args);
+	}
+
+}
